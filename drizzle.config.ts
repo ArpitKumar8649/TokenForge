f92f9ea8db1ca35acbfd9ac35b3b0c085e36a807
clashofclanshellfirehelix4-debugin <clashofclanshellfirehelix4@gmail.com>
2026-08-29 11:58:05 +0000

    Add TiDB Cloud SSL support and bump drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,20 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is required to run drizzle commands");
 }
 
+// Parse connection URL for TiDB SSL support
+const url = new URL(connectionString);
+const sslEnabled = url.hostname.includes("tidbcloud.com");
+
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
     url: connectionString,
+    ...(sslEnabled ? {
+      connection: {
+        ssl: { rejectUnauthorized: true },
+      },
+    } : {}),
   },
 });
